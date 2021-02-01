ebe67eaf5e1b83c6aa721ba99174a23508e93686
angelaleoncarmona <77341088+angelaleoncarmona@users.noreply.github.com>
2021-02-01 13:33:28 +0100

    carrousel con circulitos

--- js/main.js
+++ js/main.js
@@ -4,10 +4,19 @@ const imagenes = ['img/foto-uno.jpeg', 'img/foto-dos.jpeg', 'img/foto-tres.jpeg'
 const objetoImg = document.querySelector('#imagen');
 const botonRetroceder = document.querySelector('#retroceder');
 const botonAvanzar = document.querySelector('#avanzar');
+const templateCiruclo = document.querySelector('#template-circulo').content.firstElementChild;
+const circulos = document.querySelector('#circulos');
 let pagina = 1;
 
-// Funciones
 
+
+// Funciones
+// vincularemos esto con cada circulo
+function cambiarPagina(nuevaPagina) {
+    pagina = nuevaPagina;
+    render();
+}
+// Avanzar con boton
 function avanzarFoto() {
     pagina = pagina + 1;
     // Verificamos que no hemos alcanzado el límite, en caso de tenerlo, restaurarlo
@@ -16,7 +25,7 @@ function avanzarFoto() {
     }
     render();
 }
-
+// Retroceder con boton
 function retrocederFoto() {
     pagina = pagina - 1;
     // Verificamos que no hemos alcanzado el límite, en caso de tenerlo, restaurarlo
@@ -26,8 +35,27 @@ function retrocederFoto() {
     render();
 }
 
+
 function render() {
-    objetoImg.setAttribute('src', imagenes[pagina - 1])
+    // Imagen
+    objetoImg.setAttribute('src', imagenes[pagina - 1]);
+    // Circulitos
+    circulos.textContent = '';
+    imagenes.forEach(function (imagen, indice) {
+        // Creamos
+        const nuevoCirculo = templateCiruclo.cloneNode(true);
+        // Anyadimos Eventos
+        nuevoCirculo.addEventListener('click', function () {
+            cambiarPagina(indice +1);
+        });
+        // Marcamos el que coincide con la página
+        if (pagina === indice +1) {
+        nuevoCirculo.setAttribute('checked', true);
+        }
+        // Mostramos
+        circulos.appendChild(nuevoCirculo);
+    });
+
 }
 
 
